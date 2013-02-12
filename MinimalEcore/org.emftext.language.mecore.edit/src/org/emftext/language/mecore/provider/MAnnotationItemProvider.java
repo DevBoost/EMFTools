9db592b3bc9a548406c632af3885f9007067e347
Mirko Seifert <mirko.seifert@devboost.de>
2013-02-12 10:03:45 +0100

    removed unused import

--- MinimalEcore/org.emftext.language.mecore.edit/src/org/emftext/language/mecore/provider/MAnnotationItemProvider.java
+++ MinimalEcore/org.emftext.language.mecore.edit/src/org/emftext/language/mecore/provider/MAnnotationItemProvider.java
@@ -22,11 +22,8 @@ import java.util.List;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
-
 import org.eclipse.emf.common.util.ResourceLocator;
-
 import org.eclipse.emf.ecore.EStructuralFeature;
-
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
 import org.eclipse.emf.edit.provider.IItemLabelProvider;
@@ -34,10 +31,8 @@ import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
 import org.eclipse.emf.edit.provider.IItemPropertySource;
 import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
 import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
-import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
 import org.eclipse.emf.edit.provider.ItemProviderAdapter;
 import org.eclipse.emf.edit.provider.ViewerNotification;
-
 import org.emftext.language.mecore.MAnnotation;
 import org.emftext.language.mecore.MecoreFactory;
 import org.emftext.language.mecore.MecorePackage;
